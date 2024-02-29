d51cadab817e3f49c40d0cb8ec7d980eb937b04d
AlejoRiwi <160673766+AlejoRiwi@users.noreply.github.com>
2024-02-29 12:45:49 -0500

    Add files via upload

--- TALLER MYSQL/punto 2/Bank.sql
+++ TALLER MYSQL/punto 2/Bank.sql
@@ -10,7 +10,7 @@ create table sucursales (
 	id_codigo int primary key,
     ubicacion varchar (50) not null,
     ciudad varchar (50) not null,
-    telefono varchar (10) not null
+    telefono varchar (25) not null
 );
 
 create table empleados (
@@ -18,7 +18,13 @@ create table empleados (
     nombre varchar (50) not null,
     fecha_nac date not null,
     genero varchar (10) check(genero in ("F", "M", "Otro")),
-    telefono  varchar (10) not null,
+    telefono  varchar (25) not null,
     salario  decimal(8,2) not null,
     nro_hijos int not null
+);
+
+create table cargo (
+    codigo int primary key,
+    nombre  varchar (30) not null,
+    descripcion varchar (60) not null
 );
